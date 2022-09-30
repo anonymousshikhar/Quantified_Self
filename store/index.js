3e6969fab4d84744d8672428cae32f38c0b03d76
Akshay Shikhar Srivastava <88092644+anonymousshikhar@users.noreply.github.com>
2022-09-30 16:23:36 +0530

    Add files via upload

--- store/index.js
+++ store/index.js
@@ -1,1 +1,29 @@
+import { createStore } from "vuex";
 
+export default createStore({
+  state:{
+    isSettingOpened: false,
+    isDetailsOpened: false,
+    trackerId: '',
+    tracker_name: '',
+    tracker_desc: '',
+    tracker_pri_question: ''
+  },
+  mutations: {
+    setIsSettingOpened(state, value){
+      state.isSettingOpened = value
+    },
+    setIsDetailsOpened(state, value){
+      state.isDetailsOpened = value
+    },
+    getTrackerdetails(state, value){
+      state.trackerId = value.tid
+      state.tracker_name = value.tname
+      state.tracker_desc = value.tdesc
+      state.tracker_pri_question = value.tques
+    },
+    getTrackerQues(state, value){
+      state.tracker_pri_question = value
+    }
+  }
+})
